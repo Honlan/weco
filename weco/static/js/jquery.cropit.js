84841d111712eb2a5f47dbe0e1a2daaf6d76fa00
Honlan <493722771@qq.com>
2015-09-04 16:44:40 +0800

    canvas test

--- weco/static/js/jquery.cropit.js
+++ weco/static/js/jquery.cropit.js
@@ -756,8 +756,8 @@
                             var preresizedImage = this.preresizeImage(this.image, zoomedSize.w, zoomedSize.h);
                             canvasContext.drawImage(preresizedImage, this.offset.x * exportZoom, this.offset.y * exportZoom, zoomedSize.w, zoomedSize.h);
 
-                            // return canvas.toDataURL(exportOptions.type, exportOptions.quality);
-                            return this.imageSrc;
+                            console.log(canvas.toDataURL(exportOptions.type, exportOptions.quality))
+                            return canvas.toDataURL(exportOptions.type, exportOptions.quality);
                         }
                     }, {
                         key: 'preresizeImage',
